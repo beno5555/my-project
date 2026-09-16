b598c79ffee464e546c51b8ac9b2b2c86ccd61a6
beno5555 <159558340+beno5555@users.noreply.github.com>
2026-09-16 13:34:20 +0400

    added html validation

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const h1 = document.getElementById('hello');
+
+if (h1) {
+    h1.textContent = 'hello from main.js';
+    h1.style.color = 'orange';
+    h1.style.fontSize = '50px';
+    h1.style.textTransform = 'uppercase';
+}
